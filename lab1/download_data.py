e813b00ce4b705d490d562902247421a9c61b0a6
Nguyen Que Phong <Ynwawithpogct@gmail.com>
2025-06-15 01:40:03 +0700

    First update download_data.py function: animals-10

--- lab1/download_data.py
+++ lab1/download_data.py
@@ -2,18 +2,22 @@ import os
 import shutil
 import time
 from huggingface_hub import snapshot_download
+from dotenv import load_dotenv
 
 
 def download_animals10(data_path = "data", temp_path = "temp"):
-    if os.path.exists(data_path) and os.path.isdir(data_path) and not os.listdir(data_path):
+    if os.path.exists(data_path) and os.path.isdir(data_path) and len(os.listdir(data_path))>0:
+        print("Dataset already exists")
         return 
     os.makedirs(data_path, exist_ok=True)
     os.makedirs(temp_path, exist_ok=False)
+    load_dotenv()
+    token = os.getenv("HUGGINGFACE_HUB_TOKEN")
     local_dir = snapshot_download(
         repo_id="dgrnd4/animals-10", 
         repo_type="dataset", 
         local_dir=temp_path,
-        token=os.environ["HUGGINGFACE_HUB_TOKEN"]
+        token=token
     )
 
     print(f"Dữ liệu đã tải về: {local_dir}")
